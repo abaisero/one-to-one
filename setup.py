0466bc9cb93221d7dffe8f1e39264f81033e6bff
abaisero <andrea.baisero@gmail.com>
2019-09-12 19:22:31 -0400

    change version number (down), verify correct python version (3.7), add requirements.txt

--- setup.py
+++ setup.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python
 
-import os
 import sys
 
 from setuptools import setup
 
-
-PYTHON_CURRENT = sys.version_info[0]
-PYTHON_REQUIRED = 3
-
-if PYTHON_CURRENT < PYTHON_REQUIRED:
-    sys.stderr.write("Python >= 3 is required.")
+if sys.version_info < (3, 7):
+    sys.stderr.write('Python >= 3.7 is required.')
     sys.exit(1)
 
 
 setup(
     name='indextools',
-    version='1.0.0',
+    version='0.1.0',
     description='indextools - bijective mapping between semantic value and index.',
     author='Andrea Baisero',
     author_email='andrea.baisero@gmail.com',
     url='https://github.com/bigblindbais/indextools',
-
     packages=['indextools'],
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     test_suite='tests',
-
     install_requires=['numpy'],
     license='MIT',
 )
